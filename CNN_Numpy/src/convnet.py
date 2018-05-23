9f32ed3634f189d3f1014f52f40f8cfab279e4c6
ADITYA DANGI <adityadangi1@gmail.com>
2018-05-23 22:00:05 +0530

    Initial commit

---
+++ CNN_Numpy/src/convnet.py
@@ -0,0 +1,95 @@
+import numpy as np
+from loss import cross_entropy, dcross_entropy
+from steps import Steps
+
+
+class Conv(object):
+
+    def __init__(self, num_filters, classes, hidden_units=256):
+
+        self.params = {}
+        self.num_filters = num_filters
+        self.classes = classes
+        self.hidden_units = hidden_units
+        self.step = Steps()
+        self.initialize_model()
+
+    def initialize_model(self):
+
+        self.params = dict(
+            w1=np.random.randn(self.num_filters, 1, 3, 3) / np.sqrt(self.num_filters / 2.0),
+            w2=np.random.randn(self.num_filters * 14 * 14, self.hidden_units) / np.sqrt(self.num_filters * 14 * 14 / 2.0),
+            w3=np.random.randn(self.hidden_units, self.classes) / np.sqrt(self.hidden_units / 2.0),
+            b1=np.zeros((self.num_filters, 1)) + 0.002,
+            b2=np.zeros((1, self.hidden_units)) + 0.001,
+            b3=np.zeros((1, self.classes)) + 0.06
+        )
+        print("Model Params")
+        for i, j in self.params.items():
+            print(i, ": ", j.shape)
+
+    def train(self, x_train, y_train):
+
+        y_pred, cache = self.forward(x_train)
+        loss = cross_entropy(y_pred, y_train)
+        grad = self.backward(y_pred, y_train, cache)
+
+        return grad, loss
+
+    def forward(self, x):
+
+        num_samples = x.shape[0]
+        h_1, h_1_cache = self.step.conv_forward(x, self.params["w1"], self.params["b1"], stride=1, padding=1)
+        h_1, relu_cache_1 = self.step.relu_forward(h_1)
+        pool, pool_cache = self.step.maxpool_forward(h_1, size=2, stride=2)
+
+        h_2 = pool.ravel().reshape(num_samples, -1)
+
+        fc_1, fc_cache_1 = self.step.fc_forward(h_2, self.params["w2"], self.params["b2"])
+        fc_1, relu_cache_2 = self.step.relu_forward(fc_1)
+
+        fc_2, fc_cache_2 = self.step.fc_forward(fc_1, self.params["w3"], self.params["b3"])
+        cache = (x, h_1_cache, relu_cache_1, pool, pool_cache, fc_cache_1, relu_cache_2, fc_cache_2)
+        return fc_2, cache
+
+    def backward(self, y_pred, y_train, cache):
+
+        x, h_1_cache, relu_cache_1, pool, pool_cache, fc_cache_1, relu_cache_2, fc_cache_2 = cache
+
+        grad = dcross_entropy(y_pred, y_train)
+
+        dh3, dw3, db3 = self.step.fc_backward(grad, fc_cache_2)
+        dh3 = self.step.relu_backward(dh3, relu_cache_2)
+
+        dh2, dw2, db2 = self.step.fc_backward(dh3, fc_cache_1)
+
+        dh2 = dh2.ravel().reshape(pool.shape)
+
+        dpool = self.step.maxpool_backward(dh2, pool_cache)
+
+        dh1 = self.step.relu_backward(dpool, relu_cache_1)
+
+        dh, dw1, db1 = self.step.conv_backward(dh1, h_1_cache)
+
+        grads = dict(
+            w1=dw1,
+            w2=dw2,
+            w3=dw3,
+            b1=db1,
+            b2=db2,
+            b3=db3
+        )
+
+        return grads
+
+    def predict(self, x):
+        pred, _ = self.forward(x)
+        return np.argmax(self.softmax(pred), axis=1)
+
+    def softmax(self, x):
+
+        reg = (x.T - np.max(x, axis=1)).T
+        ex = np.exp(reg)
+        ex = ex / np.sum(ex, axis=1).reshape(-1, 1)
+        return ex
+
---
+++ CNN_Numpy/src/steps.py
@@ -0,0 +1,97 @@
+import numpy as np
+from im2col import *
+
+
+class Steps(object):
+
+    def fc_forward(self, x, w, b):
+
+        layer_out = np.dot(x, w) + b
+        cache = (w, x)
+
+        return layer_out, cache
+
+    def fc_backward(self, dout, cache):
+
+        w, h = cache
+        dw = np.dot(h.T, dout)
+        db = np.sum(dout, axis=0)
+        dh = np.dot(dout, w.T)
+
+        return dh, dw, db
+
+    def relu_forward(self, x):
+
+        return np.maximum(x, 0), x
+
+    def relu_backward(self, dout, cache):
+
+        dout[cache<0] = 0
+
+        return dout
+
+    def conv_forward(self, x, w, b, padding, stride):
+
+        n_filter, d_filter, h_filter, w_filter = w.shape
+        n_x, d_x, h_x, w_x = x.shape
+
+        h_out = int((h_x - h_filter + 2 * padding) / stride + 1)
+        w_out = int((w_x - w_filter + 2 * padding) / stride + 1)
+
+        x_col = im2col_indices(x, h_filter, h_filter, padding, stride)
+        w_col = w.reshape(n_filter, -1)
+
+        layer_out = np.dot(w_col, x_col) + b
+        layer_out = layer_out.reshape(n_filter, h_out, w_out, n_x)
+        layer_out = layer_out.transpose(3, 0, 1, 2)
+
+        cache = (x, w, b, stride, padding, x_col)
+
+        return layer_out, cache
+
+    def conv_backward(self, dout, cache):
+
+        x, w, b, stride, padding, x_col = cache
+        n_filter, d_filter, h_filter, w_filter = w.shape
+
+        db = np.sum(dout, axis=(0, 2, 3)).reshape(n_filter, -1)
+        dout_reshaped = dout.transpose(1, 2, 3, 0).reshape(n_filter, -1)
+        dw = np.dot(dout_reshaped, x_col.T)
+        dw = dw.reshape(w.shape)
+
+        w_reshape = w.reshape(n_filter, -1)
+        dx_col = np.dot(w_reshape.T, dout_reshaped)
+        dx = col2im_indices(dx_col, x.shape, h_filter, w_filter, padding, stride)
+
+        return dx, dw, db
+
+    def maxpool_forward(self, x, size=2, stride=2):
+
+        n_x, d_x, h_x, w_x = x.shape
+        h_out = int((h_x - size) / stride + 1)
+        w_out = int((w_x - size) / stride + 1)
+
+        x_reshaped = x.reshape(n_x * d_x, 1, h_x, w_x)
+        x_col = im2col_indices(x_reshaped, size, size, 0, 2)
+
+        max_idx = np.argmax(x_col, axis=0)
+        layer_out = x_col[max_idx, range(max_idx.size)]
+        layer_out = layer_out.reshape(h_out, w_out, n_x, d_x)
+        layer_out = layer_out.transpose(2, 3, 0, 1)
+
+        cache = (x, size, stride, x_col, max_idx)
+
+        return layer_out, cache
+
+    def maxpool_backward(self, dout, cache):
+
+        x, size, stride, x_col, max_idx = cache
+        n_x, d_x, h_x, w_x = x.shape
+
+        dx_col = np.zeros_like(x_col)
+        dout_col = dout.transpose(2, 3, 0, 1).ravel()
+
+        dx_col[max_idx, range(dout_col.size)] = dout_col
+        dx = col2im_indices(dx_col, (n_x*d_x, 1, h_x, w_x), size, size, 0, stride)
+
+        return dx.reshape(x.shape)
